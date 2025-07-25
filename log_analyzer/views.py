f3a38c24ce1cf934124b79d1f409855736171ab5
OmarDIOP0 <odiop6170@gmail.com>
2025-07-25 00:35:58 +0000

    bugs

--- log_analyzer/views.py
+++ log_analyzer/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .models import AuthLog
 from django.shortcuts import redirect
-import pandas as pd
-from datetime import datetime
 
 def dashboard(request):
     total_logs = AuthLog.objects.count()
@@ -19,24 +17,8 @@ def log_list(request):
     logs = AuthLog.objects.all().order_by('-timestamp')
     return render(request, 'log_analyzer/log_list.html', {'logs': logs})
 
-
 def upload_logs(request):
     if request.method == 'POST' and request.FILES.get('log_file'):
-        # Lecture du fichier CSV
-        df = pd.read_csv(request.FILES['log_file'])
-        
-        # Transformation des données
-        for _, row in df.iterrows():
-            AuthLog.objects.create(
-                timestamp=datetime.strptime(row['timestamp'], '%Y-%m-%d %H:%M:%S'),
-                category=row['category'],
-                source_ip=row['source_ip'],
-                username=row.get('username'),
-                status=row['status'],
-                message=row['message'],
-                raw_log=row['raw_log'],
-                is_malicious=False  # Vous ajouterez la prédiction ici
-            )
-        
+        # Ici vous ajouterez le code pour traiter le fichier
         return redirect('dashboard')
     return render(request, 'log_analyzer/upload.html')
